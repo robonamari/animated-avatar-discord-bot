a4d4e38724e4413f0a810ca7e980b502ecf4c261
robonamari <ROBONAMARI@GMAIL.COM>
2024-07-24 07:42:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import requests
 import base64
 
-token = ''
-avatar_path = './Animated-Avatar.gif'
+token = ""
+avatar_path = "./Animated-Avatar.gif"
 
 try:
-    with open(avatar_path, 'rb') as file:
-        new_avatar = base64.b64encode(file.read()).decode('utf-8')
+    with open(avatar_path, "rb") as file:
+        new_avatar = base64.b64encode(file.read()).decode("utf-8")
 
     headers = {
-        'Authorization': f'Bot {token}',
-        'Content-Type': 'application/json'
+        "Authorization": f"Bot {token}",
+        "Content-Type": "application/json"
     }
 
-    data = {
-        'avatar': f'data:image/gif;base64,{new_avatar}'
+    json = {
+        "avatar": f"data:image/gif;base64,{new_avatar}"
     }
 
-    response = requests.patch('https://discord.com/api/v10/users/@me', headers=headers, json=data)
+    response = requests.patch("https://discord.com/api/v10/users/@me", headers=headers, json=json)
 
     if response.ok:
-        print('Avatar Updated!')
+        print("Avatar Updated!")
     else:
-        print('Failed to Update Avatar:', response.status_code)
-        print('Response body:', response.text)
+        print("Failed to Update Avatar:", response.status_code)
+        print("Response body:", response.text)
 
 except Exception as error:
-    print('There is an Error here:', error)
+    print("There is an Error here:", error)
