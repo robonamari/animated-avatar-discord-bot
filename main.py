25622d2eebf603797906bef0404c92ac738de721
robonamari <ROBONAMARI@GMAIL.COM>
2025-01-26 13:24:52 +0330

    Code Formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import requests
 import base64
+
+import requests
 
 token = ""
 avatar_path = "./Animated-Avatar.gif"
@@ -8,16 +9,13 @@ try:
     with open(avatar_path, "rb") as file:
         new_avatar = base64.b64encode(file.read()).decode("utf-8")
 
-    headers = {
-        "Authorization": f"Bot {token}",
-        "Content-Type": "application/json"
-    }
+    headers = {"Authorization": f"Bot {token}", "Content-Type": "application/json"}
 
-    json = {
-        "avatar": f"data:image/gif;base64,{new_avatar}"
-    }
+    json = {"avatar": f"data:image/gif;base64,{new_avatar}"}
 
-    response = requests.patch("https://discord.com/api/v10/users/@me", headers=headers, json=json)
+    response = requests.patch(
+        "https://discord.com/api/v10/users/@me", headers=headers, json=json
+    )
 
     if response.ok:
         print("Avatar Updated!")
