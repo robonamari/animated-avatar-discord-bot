65e1af65bba6c15cce480522a74efcb40013e366
robonamari <ROBONAMARI@GMAIL.COM>
2025-03-12 12:40:15 +0330

    Remove Animated-Avatar.gif and refactor avatar update logic in main.py

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ avatar_path = "./Animated-Avatar.gif"
 try:
     with open(avatar_path, "rb") as file:
         new_avatar = base64.b64encode(file.read()).decode("utf-8")
-
-    headers = {"Authorization": f"Bot {token}", "Content-Type": "application/json"}
-
-    json = {"avatar": f"data:image/gif;base64,{new_avatar}"}
-
     response = requests.patch(
-        "https://discord.com/api/v10/users/@me", headers=headers, json=json
+        "https://discord.com/api/v10/users/@me",
+        headers={"Authorization": f"Bot {token}", "Content-Type": "application/json"},
+        json={"avatar": f"data:image/gif;base64,{new_avatar}"},
     )
-
     if response.ok:
         print("Avatar Updated!")
     else:
